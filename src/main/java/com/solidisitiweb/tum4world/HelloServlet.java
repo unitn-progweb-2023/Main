f4d040588b6d54e763a62cb04e54126a3329dcbb
minaCod3 <davide.minatel@gmail.com>
2023-05-15 23:33:47 +0200

    maybe here we are! (Add some biutiful stail)

--- src/main/java/com/solidisitiweb/tum4world/HelloServlet.java
+++ src/main/java/com/solidisitiweb/tum4world/HelloServlet.java
@@ -3,6 +3,7 @@ package com.solidisitiweb.tum4world;
 import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import javax.swing.text.html.parser.Parser;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
@@ -15,9 +16,23 @@ public class HelloServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
+        /*
+        // per convertire in stringa il contenuto del file header.html
+        // ispirato da: https://www.geeksforgeeks.org/java-program-to-extract-content-from-a-html-document/
+        StringBuilder headerString = new StringBuilder();
+        BufferedReader buffer = new BufferedReader((new FileReader("header.html"))); // not found aaaa
+
+        String aux;
+        while((aux = buffer.readLine()) != null) { headerString.append(aux); }
+        */
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html>");
+        out.println("<head>" +
+                "    <title>Tum4World</title>" +
+                "    <link rel=\"icon\" href=\"resources/favicon.ico\">" +
+                "</head>");
+        // out.println(headerString.toString());
         out.println("<body>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,13 +1,11 @@
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <!DOCTYPE html>
 <html>
-<head>
-    <jsp:include page="header.html"/>
-</head>
+<%@ include file="header.html"%>
 <body>
-<h1><%= "Hello World!" %>
+<h1><%= "Tum4World" %>
 </h1>
 <br/>
-<a href="hello-servlet">Hello Servlet</a>
+<a href="hello-servlet">clicca qui mona</a>
 </body>
 </html>
