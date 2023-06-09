842f79935ed6cd14a211797e3549f048940595dd
DinoCappe <ludovico.cappellato@gmail.com>
2023-06-09 18:28:35 +0200

    Server mail

--- src/main/java/com/solidisitiweb/tum4world/SendEmail.java
+++ src/main/java/com/solidisitiweb/tum4world/SendEmail.java
@@ -1,5 +1,9 @@
 package com.solidisitiweb.tum4world;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.servlet.annotation.WebServlet;
@@ -7,16 +11,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import javax.mail.Authenticator;
-import java.util.Date;
 import java.util.Properties;
-import javax.mail.*;
-import javax.mail.internet.*;
 
 @WebServlet(name = "sendEmail", value = "/sendEmail")
 public class SendEmail extends HttpServlet {
 
+    private Session sessioneSMTP;
+
     public void init() {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.Tum4World.com");
+        props.put("mail.smtp.port", "587");
+        //props.put("mail.smtp.auth", "true");
+        //props.put("mail.smtp.starttls.enable", "true");
+        sessioneSMTP = Session.getInstance(props, null);
+        /* Session session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("usr", "psw");
+            }
+        });*/
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -27,19 +40,6 @@ public class SendEmail extends HttpServlet {
         String motivo = request.getParameter("motivazioni");;
         String ulterioriInfo = request.getParameter("ulteriori");;
 
-
-        Properties props = new Properties();
-        props.put("mail.smtp.host", "smtp.Tum4World.com");
-        props.put("mail.smtp.port", "587");
-        //props.put("mail.smtp.auth", "true");
-        //props.put("mail.smtp.starttls.enable", "true");
-
-        Session sessioneSMTP = Session.getInstance(props, null);
-       /* Session session = Session.getInstance(props, new Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("usr", "psw");
-            }
-        });*/
         try {
             MimeMessage msg = new MimeMessage(sessioneSMTP);
             msg.setFrom(new InternetAddress(email));
